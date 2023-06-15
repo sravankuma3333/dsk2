17c713d66ff15a01e651b5dbd8023709c590160a
sravankuma3333 <136642773+sravankuma3333@users.noreply.github.com>
2023-06-15 18:11:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "webserver2ter
-" {
+resource "aws_instance" "webserver2ter" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
